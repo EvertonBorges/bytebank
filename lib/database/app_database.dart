5017ed2ce58514d29244ef2090684e1605fc389f
EvertonBorges <evertonsoaresborges@gmail.com>
2020-02-20 10:41:39 -0400

    Criando a camada DAO

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,4 +1,4 @@
-import 'package:bytebank/models/Contact.dart';
+import 'package:bytebank/dao/contact_dao.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -8,35 +8,9 @@ Future<Database> createDatabase() async {
   return openDatabase(
     path,
     onCreate: (db, version) {
-      db.execute('CREATE TABLE contacts('
-          'id INTEGER PRIMARY KEY, '
-          'name TEXT, '
-          'accountNumber INTEGER)');
+      db.execute(ContactDao.tableSql);
     },
     version: 1,
-//      onDowngrade: onDatabaseDowngradeDelete,
   );
-}
 
-Future<int> save(Contact contact) async {
-  final Database db = await createDatabase();
-
-  final Map<String, dynamic> contactMap = Map();
-  contactMap['name'] = contact.name;
-  contactMap['accountNumber'] = contact.accountNumber;
-
-  return db.insert('contacts', contactMap);
-}
-
-Future<List<Contact>> findAll() async {
-  final Database db = await createDatabase();
-  final List<Map<String, dynamic>> results = await db.query('contacts');
-
-  final List<Contact> contacts = List();
-  for (Map<String, dynamic> result in results) {
-    final Contact contact =
-        Contact(result['id'], result['name'], result['accountNumber']);
-    contacts.add(contact);
-  }
-  return contacts;
 }
