d0a6170e52486ae1aa792c8f6e080aa4721158ae
EvertonBorges <evertonsoaresborges@gmail.com>
2020-02-24 11:27:48 -0400

    Apresentando mensagem de feedback

--- lib/screens/transaction_form.dart
+++ lib/screens/transaction_form.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/components/response_dialog.dart';
 import 'package:bytebank/components/transaction_auth_dialog.dart';
 import 'package:bytebank/http/webclients/transaction_webclient.dart';
 import 'package:bytebank/models/Contact.dart';
@@ -93,10 +94,14 @@ class _TransactionFormState extends State<TransactionForm> {
   ) async {
     _transactionWebClient.save(transactionCreated, password).then((transaction) {
       if (transaction != null) {
-        Navigator.pop(context);
+        showDialog(context: context, builder: (contextDialog) {
+          return SuccessDialog('successful transaction');
+        }).then((value) => Navigator.pop(context));
       }
     }).catchError((e){
-      print(e);
-    });
+      showDialog(context: context, builder: (contextDialog) {
+        return FailureDialog(e.message);
+      });
+    }, test: (e) => e is Exception);
   }
 }
